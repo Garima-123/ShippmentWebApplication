ba087e5131a0a7fcb1a33c7a9c321c15596b80cf
garim <garim@DESKTOP-S0L74HO>
2021-11-28 19:52:35 +0530

    Shipment List, Create , Delete

---
+++ ShippmentWebApplication/Controllers/AddController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ShippmentWebApplication.Models;
+namespace ShippmentWebApplication.Controllers
+{
+    public class AddController : Controller
+    {
+        // GET: Add
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Save()
+        {
+            return View("~/Views/Add/Save.cshtml");
+        }
+
+        [HttpPost]
+        public ActionResult SaveSender(Sender sender)
+        {
+            Shippment_DBEntities dbContext = new Shippment_DBEntities();
+
+            dbContext.Senders.Add(sender);
+            dbContext.SaveChanges();
+
+            //Fetch the CustomerId returned via SCOPE IDENTITY.
+            int id = sender.SenderId;
+
+            return RedirectToAction("Index", "Sender", new { id = id });
+        }
+    }
+}
